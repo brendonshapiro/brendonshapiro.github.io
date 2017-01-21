923480156148e2337cd104070503f959ab344b83
Eric Shapiro <eshapir@us.ibm.com>
2017-01-21 16:04:32 -0500

    Completed 'About', 'Calendar', and 'Contact' sections

--- js/theme.js
+++ js/theme.js
@@ -28,6 +28,38 @@
         offset: {
             top: 100
         }
-    })
-
+    });
+    
+    $('#aboutbutton').click(function(){
+        $(this).remove();
+    });
 })(jQuery); // End of use strict
+
+(function() {
+    
+    var html = $('html'),
+        H = html.outerHeight(true),
+        S = $(window).scrollTop(),
+        P = S/H,
+        toggle = $('input#toggle');
+    
+    $(window).scroll(function() {
+        S = $(window).scrollTop();
+        P = S/H;
+    });
+    
+    $(window).resize(function() {
+        if ( toggle.is(':checked') ) {
+            H = html.outerHeight(true);
+            $(window).scrollTop(P*H);
+        }
+    });
+    
+    toggle.change(function() {
+        H = html.outerHeight(true);
+        S = $(window).scrollTop();
+        P = S/H;
+    });
+    
+})();
+
